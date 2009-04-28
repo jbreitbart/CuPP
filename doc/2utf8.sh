9a1ae17f1e91198e8ef4730eb75ee83739c28e54
jbreitbart <jbreitbart@a7bbc9c9-2611-0410-a209-afa16a0fa7d6>
2009-04-28 08:00:36 +0000

    Cleanup

--- doc/2utf8.sh
+++
@@ -1,3 +0,0 @@
-#bin/bash
-
-sed -i -e "s/charset=iso-8859-1/charset=utf-8/g" html/*html
--- test/64bit/kernel_t.h
+++
@@ -1,7 +0,0 @@
-#ifndef kernel_t_H
-#define kernel_t_H
-
-// implemented in the .cu file
-const char* get_kernel();
-
-#endif
